2169e71b21cb153ffcb744302b9985b75a33a0cf
Furkanayaan <ayanf16@itu.edu.tr>
2022-09-12 17:08:35 +0300

    WeirdAlgorithm

---
+++ WeirdAlgorithm/WeirdAlgorithm/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WeirdAlgorithm
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Sayı Giriniz");
+            string sayi = Console.ReadLine();
+            int currentNumber = Int32.Parse(sayi);
+            while (currentNumber != 1)
+            {
+                if (currentNumber % 2 == 1)
+                {
+                    currentNumber = currentNumber * 3 + 1;
+                    Console.WriteLine(currentNumber);
+
+                }
+
+                if (currentNumber % 2 == 0)
+                {
+                    currentNumber = currentNumber / 2;
+                    Console.WriteLine(currentNumber);
+                }
+
+            }
+
+            
+        }
+
+    }
+}
